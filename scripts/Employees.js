301086739f0825c8bbb86120be3e07298ecc87cf
Christian Suriano <cratea113@gmail.com>
2022-06-13 11:22:32 -0500

    new and improved dog walker

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,38 @@
 import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js" 
 
+const orders = getOrders()
 const employees = getEmployees()
+
+const employeeOrders = (employee) => {
+    let fulfilledOrders = 0
+
+    for (const order of orders) {
+        if (order.employeeId === employee.id) {
+            fulfilledOrders ++;
+        }
+    }
+
+    return fulfilledOrders
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const orderCount = employeeOrders(employee)
+                    window.alert(`${employee.name} sold: ${orderCount} products `)
+                }
+            }
+        }
+    }
+)
+
 
 export const Employees = () => {
     let html = "<ul>"
